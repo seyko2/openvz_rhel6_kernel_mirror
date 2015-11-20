b7372f45911f244cdc625a14af73ccd296fc4559
seyko <seyko2@gmail.com>
2015-11-20 08:02:23 +0300

    droid/012-net_activity_stats
        CONFIG_ANDROID_PARANOID_NETWORK
        CONFIG_NET_ACTIVITY_STATS

--- security/commoncap.c
+++ security/commoncap.c
@@ -30,6 +30,10 @@
 #include <linux/syslog.h>
 #include <linux/personality.h>
 
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+#include <linux/android_aid.h>
+#endif
+
 /*
  * If a non-root user executes a setuid-root binary in
  * !secure(SECURE_NOROOT) mode, then we raise capabilities.
@@ -76,6 +80,12 @@ int cap_netlink_send(struct sock *sk, struct sk_buff *skb)
 int cap_capable(struct task_struct *tsk, const struct cred *cred, int cap,
 		int audit)
 {
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+	if (cap == CAP_NET_RAW && in_egroup_p(AID_NET_RAW))
+		return 0;
+	if (cap == CAP_NET_ADMIN && in_egroup_p(AID_NET_ADMIN))
+		return 0;
+#endif
 	return cap_raised(cred->cap_effective, cap) ? 0 : -EPERM;
 }
 
