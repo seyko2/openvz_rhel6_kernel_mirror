98ca079bdd5f240800846f86361f3546e8b69185
seyko <seyko2@gmail.com>
2015-11-20 06:01:03 +0300

    r08520-sp5100_tco_watchdog2.patch
        small fix

--- drivers/watchdog/sp5100_tco.c
+++ drivers/watchdog/sp5100_tco.c
@@ -303,7 +303,8 @@ static struct miscdevice sp5100_tco_miscdev = {
  * register a pci_driver, because someone else might
  * want to register another driver on the same PCI id.
  */
-static DEFINE_PCI_DEVICE_TABLE(sp5100_tco_pci_tbl) = {
+
+static __devinitconst DEFINE_PCI_DEVICE_TABLE(sp5100_tco_pci_tbl) = {
 	{ PCI_VENDOR_ID_ATI, PCI_DEVICE_ID_ATI_SBX00_SMBUS, PCI_ANY_ID,
 	  PCI_ANY_ID, },
 	{ 0, },			/* End of list */
